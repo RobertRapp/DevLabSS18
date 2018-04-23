84d3de235455541e91491786ab50b60bdd8be76f
RobertRapp <rr044@hdm-stuttgart.de>
2018-04-23 17:46:19 +0200

    Some Test classes

---
+++ Event-Based-Integration/eve_implementation/src/main/java/hdm/developmentlab/ebi/eve_implementation/sessionInfoService/SessionInfoAgent.java
@@ -0,0 +1,16 @@
+package hdm.developmentlab.ebi.eve_implementation.sessionInfoService;
+
+import eventprocessing.agent.AbstractAgent;
+
+public class SessionInfoAgent{
+
+	
+
+	
+	protected void doOnInit() {
+		//this.add(interestProfile);
+		
+	}
+
+	
+}
