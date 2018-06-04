a1beff589430b83b50c62d78a5903c85574cfbfd
jonas <jonas@DESKTOP-AH5M8MP>
2018-06-04 16:32:40 +0200

    Erstellung DoInit()- und DoReceive()-Methode für Application Agent +Interessenprofil

--- Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/ActivityAgent.java
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/ActivityAgent.java
@@ -1,6 +1,16 @@
 package hdm.developmentlab.ebi.eve_implementation.activityService;
 
 import eventprocessing.agent.AbstractAgent;
+import eventprocessing.agent.exceptions.NoValidConsumingTopicException;
+import eventprocessing.demo.ShowcaseValues;
+import eventprocessing.demo.interestprofiles.DiagnosisInterestProfile;
+import eventprocessing.dispatch.NoValidInterestProfileException;
+import eventprocessing.input.kafka.ConsumerSettings;
+import eventprocessing.interestprofile.AbstractInterestProfile;
+import eventprocessing.interestprofile.predicates.statement.IsEventType;
+import hdm.developmentlab.ebi.eve_implementation.activityService.interestprofiles.TokenApplicationType;
+import hdm.developmentlab.ebi.eve_implementation.events.*;
+
 
 public class ActivityAgent extends AbstractAgent {
 
@@ -8,9 +18,34 @@ public class ActivityAgent extends AbstractAgent {
 
 	
 	protected void doOnInit() {
-		//this.add(interestProfile);
+		
+		/*
+		 * FÃ¼gt dem Agenten ein InteressenProfil hinzu Ein Agent kann mehrere
+		 * InteressenProfile besitzen
+		 */
+		try {
+			AbstractInterestProfile ip = new TokenApplicationType();
+			//ip.add(new IsEventType(ShowcaseValues.INSTANCE.Token()));
+			this.add(ip);
+		} catch (NoValidInterestProfileException e1) {
+			e1.printStackTrace();
+		}
+		/*
+		 * Angabe der Topics, die konsumiert werden sollen. Es kÃ¶nnen mehrere Topics
+		 * angegeben werden.
+		 */
+		try {
+			this.add("TokenGeneration");
+		} catch (NoValidConsumingTopicException e) {
+			e.printStackTrace();
+		}
+		this.setConsumerSettings(new ConsumerSettings(ShowcaseValues.INSTANCE.getIpKafka(),
+				ShowcaseValues.INSTANCE.getPortKafka(), "Tokens2"));
+
+	}
+		
 		
 	}
 
 	
-}
+
--- Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/interestprofiles/TokenApplicationType.java
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/interestprofiles/TokenApplicationType.java
@@ -1,8 +1,10 @@
 package hdm.developmentlab.ebi.eve_implementation.activityService.interestprofiles;
 
 
+import eventprocessing.demo.events.SpeedEvent;
 import eventprocessing.event.AbstractEvent;
 import eventprocessing.interestprofile.AbstractInterestProfile;
+import hdm.developmentlab.ebi.eve_implementation.events.TokenEvent;
 
 
 public class TokenApplicationType extends AbstractInterestProfile {
@@ -15,8 +17,19 @@ public class TokenApplicationType extends AbstractInterestProfile {
 
 
 	@Override
-	protected void doOnReceive(AbstractEvent arg0) {
-		// TODO Auto-generated method stub
+	protected void doOnReceive(AbstractEvent event) {
+	/*
+	 * Hier werden die Muster definiert, um zu prüfen, ob innerhalb ein Token vom Typ Application 
+	 * ist. 
+	 */
+		
+		if (event instanceof TokenEvent) {
+			// casten zu SpeedEvent um HÃ¶chstgeschwindigkeit auszulesen
+			SpeedEvent speedEvent = (SpeedEvent) event;
+			// Alle benÃ¶tigten Informationen werden aus dem Event entnommen
+			// e.setVelocity(speedEvent.getAverageSpeed());
+
+		}
 		
 	}
 
