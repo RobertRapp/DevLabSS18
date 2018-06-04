ca8b32f9e0e1fc1fb883df8bc52cd01abc801666
RobertRapp <rr044@hdm-stuttgart.de>
2018-06-04 16:33:06 +0200

    Erster Test um eine Kafka Message zu produzieren.

--- Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/sessionContextService/SessionContextAgent.java
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/sessionContextService/SessionContextAgent.java
@@ -1,13 +1,112 @@
 package hdm.developmentlab.ebi.eve_implementation.sessionContextService;
 
 import eventprocessing.agent.AbstractAgent;
-import eventprocessing.interestprofile.AbstractInterestProfile;
+import eventprocessing.agent.exceptions.NoValidConsumingTopicException;
+import startServices.ShowcaseValues;
+import eventprocessing.dispatch.NoValidInterestProfileException;
+import eventprocessing.input.kafka.ConsumerSettings;
+import eventprocessing.interestprofile.AbstractInterestProfile;import eventprocessing.interestprofile.predicates.AbstractPredicate;
+import eventprocessing.interestprofile.predicates.statement.HasProperty;
+
 import hdm.developmentlab.ebi.eve_implementation.events.SessionEvent;
 import hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles.*;
 
 import java.util.ArrayList;
 
+
+
 public class SessionContextAgent extends AbstractAgent {
+
+
+	protected void doOnInit() {
+		try {
+			this.add("TokenGeneration");
+		} catch (NoValidConsumingTopicException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			
+			AbstractPredicate predicate = new HasProperty("sessionStart");						
+			tokenDocumentType.add(predicate);
+			this.add(tokenDocumentType);
+			
+		} catch (NoValidInterestProfileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.setConsumerSettings(new ConsumerSettings(ShowcaseValues.INSTANCE.getIpKafka(),
+				ShowcaseValues.INSTANCE.getPortKafka(), "Tokens"));
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ArrayList<SessionEvent> getSessions() {
+		return sessions;
+	}
+
+
+	public void addSession(SessionEvent session) {
+		sessions.add(session);
+	}
+
+
+	public AbstractInterestProfile getProject() {
+		return project;
+	}
+
+
+	public void setProject(AbstractInterestProfile project) {
+		this.project = project;
+	}
+
+
+	public SessionState getSessionState() {
+		return sessionState;
+	}
+
+
+	public void setSessionState(SessionState sessionState) {
+		this.sessionState = sessionState;
+	}
+
+
+	public TimeReference getTimeReference() {
+		return timeReference;
+	}
+
+
+	public void setTimeReference(TimeReference timeReference) {
+		this.timeReference = timeReference;
+	}
+
+
+	public TokenDocumentType getTokenDocumentType() {
+		return tokenDocumentType;
+	}
+
+
+	public void setTokenDocumentType(TokenDocumentType tokenDocumentType) {
+		this.tokenDocumentType = tokenDocumentType;
+	}
+
+
+	public User getUserInfo() {
+		return userInfo;
+	}
+
+
+	public void setUserInfo(User userInfo) {
+		this.userInfo = userInfo;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 
 	ArrayList<SessionEvent> sessions = new ArrayList<SessionEvent>();
@@ -18,12 +117,7 @@ public class SessionContextAgent extends AbstractAgent {
 	User userInfo = new User();
 
 
-
-
-	protected void doOnInit() {
-
-
-	}
+	
 
 
 }
--- Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/sessionContextService/interestprofiles/TokenDocumentType.java
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/sessionContextService/interestprofiles/TokenDocumentType.java
@@ -1,8 +1,14 @@
 package hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles;
 
 
+import eventprocessing.agent.exceptions.NoValidEventException;
+import eventprocessing.agent.exceptions.NoValidTargetTopicException;
 import eventprocessing.event.AbstractEvent;
+import eventprocessing.event.Property;
 import eventprocessing.interestprofile.AbstractInterestProfile;
+import hdm.developmentlab.ebi.eve_implementation.events.SessionEvent;
+import hdm.developmentlab.ebi.eve_implementation.events.TokenEvent;
+import hdm.developmentlab.ebi.eve_implementation.sessionContextService.SessionContextAgent;
 
 
 public class TokenDocumentType extends AbstractInterestProfile {
@@ -19,9 +25,28 @@ public class TokenDocumentType extends AbstractInterestProfile {
 	 */
 
 	@Override
-	protected void doOnReceive(AbstractEvent arg0) {
-		// TODO Auto-generated method stub
+	protected void doOnReceive(AbstractEvent arg) {
 		
+		TokenEvent arg0 = (TokenEvent) arg;
+		
+		SessionContextAgent agent = (SessionContextAgent) this.getAgent(); 	
+		
+		SessionEvent session = new SessionEvent();
+		session.setId(arg0.getId());
+		session.setSessionId(arg0.getSessionID());
+		session.setSessionStart((long) arg0.getProperties().get(0).getValue());
+		agent.addSession(session);
+		session.add(new Property<String>("Report", "Ich habe ein Token erhalten, es war ein SessionStart mit um "+session.getSessionStart()));
+		
+		try {
+			agent.send(session, "test");
+		} catch (NoValidEventException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoValidTargetTopicException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
