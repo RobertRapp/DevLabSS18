3f1f7a7d9de90fc78e20114c48165ea98307f62a
JonasBirk <jonasbirk8@gmail.com>
2018-06-11 11:34:22 +0200

    ApplicationEvent-Klasse erstellt

--- Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/ActivityAgent.java
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/ActivityAgent.java
@@ -1,12 +1,21 @@
 package hdm.developmentlab.ebi.eve_implementation.activityService;
 
+import java.util.ArrayList;
+
 import eventprocessing.agent.AbstractAgent;
 import eventprocessing.agent.exceptions.NoValidConsumingTopicException;
 import eventprocessing.demo.ShowcaseValues;
 import eventprocessing.dispatch.NoValidInterestProfileException;
 import eventprocessing.input.kafka.ConsumerSettings;
 import eventprocessing.interestprofile.AbstractInterestProfile;
+import eventprocessing.interestprofile.predicates.AbstractPredicate;
+import eventprocessing.interestprofile.predicates.statement.HasProperty;
 import hdm.developmentlab.ebi.eve_implementation.activityService.interestprofiles.TokenApplicationType;
+import hdm.developmentlab.ebi.eve_implementation.events.SessionEvent;
+import hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles.SessionState;
+import hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles.TimeReference;
+import hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles.TokenDocumentType;
+import hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles.User;
 
 
 public class ActivityAgent extends AbstractAgent {
@@ -37,6 +46,17 @@ public class ActivityAgent extends AbstractAgent {
 		 * InteressenProfile besitzen
 		 */
 		try {
+			AbstractPredicate predicate = new HasProperty("application");
+			tokenDocumentType.add(predicate);
+			this.add(tokenDocumentType);
+			
+			/*
+			 * Beinhaltet Tokenevent eine Application? 
+			 * ApplicationEvent-Objekt erzeugen 
+			 * Wenn ja, Event erzeugen, welches Link zur Application bei DR "abfragt"
+			 * 
+			 */
+			
 			AbstractInterestProfile ip = new TokenApplicationType();
 			//ip.add(new IsEventType(ShowcaseValues.INSTANCE.Token()));
 			this.add(ip);
@@ -48,6 +68,56 @@ public class ActivityAgent extends AbstractAgent {
 				ShowcaseValues.INSTANCE.getPortKafka(), "Tokens2"));
 
 	}
+	
+	
+	public ArrayList<SessionEvent> getSessions() {
+		return sessions;
+	}
+	public void setSessions(ArrayList<SessionEvent> sessions) {
+		this.sessions = sessions;
+	}
+	public AbstractInterestProfile getProject() {
+		return project;
+	}
+	public void setProject(AbstractInterestProfile project) {
+		this.project = project;
+	}
+	public SessionState getSessionState() {
+		return sessionState;
+	}
+	public void setSessionState(SessionState sessionState) {
+		this.sessionState = sessionState;
+	}
+	public TimeReference getTimeReference() {
+		return timeReference;
+	}
+	public void setTimeReference(TimeReference timeReference) {
+		this.timeReference = timeReference;
+	}
+	public TokenDocumentType getTokenDocumentType() {
+		return tokenDocumentType;
+	}
+	public void setTokenDocumentType(TokenDocumentType tokenDocumentType) {
+		this.tokenDocumentType = tokenDocumentType;
+	}
+	public User getUserInfo() {
+		return userInfo;
+	}
+	public void setUserInfo(User userInfo) {
+		this.userInfo = userInfo;
+	}
+
+
+
+
+	ArrayList<SessionEvent> sessions = new ArrayList<SessionEvent>();
+	AbstractInterestProfile project;
+	SessionState sessionState = new SessionState();
+	TimeReference timeReference = new TimeReference();
+	TokenDocumentType tokenDocumentType = new TokenDocumentType();
+	User userInfo = new User();
+	
+	
 		
 		
 	}
---
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/events/ApplicationEvent.java
@@ -0,0 +1,33 @@
+package hdm.developmentlab.ebi.eve_implementation.events;
+
+public class ApplicationEvent {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String ApplicationID;
+	private String ApplicationName;
+	private String Link;
+	
+	
+	public String getApplicationID() {
+		return ApplicationID;
+	}
+	public void setApplicationID(String applicationID) {
+		ApplicationID = applicationID;
+	}
+	public String getApplicationName() {
+		return ApplicationName;
+	}
+	public void setApplicationName(String applicationName) {
+		ApplicationName = applicationName;
+	}
+	public String getLink() {
+		return Link;
+	}
+	public void setLink(String link) {
+		Link = link;
+	}
+
+	
+	
+}
