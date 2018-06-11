20cb5be86e371f80af999460a9917ecf300eca27
JonasBirk <jonasbirk8@gmail.com>
2018-06-11 13:53:57 +0200

    Anpassung TokenApplicationType

--- Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/interestprofiles/TokenApplicationType.java
+++ Event-Based-Integration/eve_implementation/src/hdm/developmentlab/ebi/eve_implementation/activityService/interestprofiles/TokenApplicationType.java
@@ -18,6 +18,7 @@ import eventprocessing.utils.factory.FactoryProducer;
 import eventprocessing.utils.factory.FactoryValues;
 import eventprocessing.utils.factory.LoggerFactory;
 import hdm.developmentlab.ebi.eve_implementation.events.ApplicationEvent;
+import hdm.developmentlab.ebi.eve_implementation.events.DocumentRequestEvent;
 import hdm.developmentlab.ebi.eve_implementation.events.TokenEvent;
 
 
@@ -42,6 +43,7 @@ public class TokenApplicationType extends AbstractInterestProfile {
 	protected void doOnReceive(AbstractEvent event) {
 		// Erzeugt über die Factory ein neues Event
 		ApplicationEvent e = (ApplicationEvent) eventFactory.createEvent("????");
+		DocumentRequestEvent dr = (DocumentRequestEvent) eventFactory.createEvent("????");
 		
 		// Prüfe ob das empfangene Event vom Typ TokenEvent ist und eine Application beinhaltet
 		//HIER MIT PREDICATES IN IF CONDITION ARBEITEN! 
@@ -53,16 +55,42 @@ public class TokenApplicationType extends AbstractInterestProfile {
 				//e.setApplicationName(tokenEvent.getChunkSemantic());
 				e.setLink("missing");
 				
+				// Sendet das Event an DR (welches Topic ???) 
+				try {
+					getAgent().send(e, "DR Topic ???");
+				} catch (NoValidEventException e1) {
+					LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+				} catch (NoValidTargetTopicException e1) {
+					LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+				}
+				
 		}
 		
-		// Sendet das Event an DR (welches Topic ???) 
-		try {
-			getAgent().send(e, "DR Topic ???");
-		} catch (NoValidEventException e1) {
-			LOGGER.log(Level.WARNING, () -> String.format("%s", e));
-		} catch (NoValidTargetTopicException e1) {
-			LOGGER.log(Level.WARNING, () -> String.format("%s", e));
-		}
+		//Handelt es sich um eine Aktivität? Dann muss daraus eine Dokumentenabfrage generiert werden
+		//Bedingung fehl noch 
+		if (event instanceof TokenEvent) {
+			// casten um Type auszulesen
+			TokenEvent tokenEvent = (TokenEvent) event;
+			// Alle benötigten Informationen werden aus dem Event entnommen
+			
+			dr.setToken(tokenEvent);
+			
+			//Tokeninfos werden bei Bedarf um aktuellen Sessioncontext ergänzt 
+			
+			//if (tokenEvent.getProperties().get(arg0))
+			
+			// Sendet das Event an DR (welches Topic ???) 
+			try {
+				getAgent().send(e, "DR Topic ???");
+			} catch (NoValidEventException e1) {
+				LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+			} catch (NoValidTargetTopicException e1) {
+				LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+			}
+			
+	}
+		
+		
 		
 	}
 
