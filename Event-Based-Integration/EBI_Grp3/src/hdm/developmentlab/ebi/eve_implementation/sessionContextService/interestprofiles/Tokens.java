a24dc1a83957e5f626c3a00fd4dfa6f5cb43e842
RobertRapp <rr044@hdm-stuttgart.de>
2018-06-27 13:23:50 +0200

    Message Of interest meldet sich jetzt immer wenn ein Event über ihn läuft

--- Event-Based-Integration/EBI_Grp3/src/hdm/developmentlab/ebi/eve_implementation/sessionContextService/interestprofiles/Tokens.java
+++ Event-Based-Integration/EBI_Grp3/src/hdm/developmentlab/ebi/eve_implementation/sessionContextService/interestprofiles/Tokens.java
@@ -1,8 +1,14 @@
 package hdm.developmentlab.ebi.eve_implementation.sessionContextService.interestprofiles;
 
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import eventprocessing.agent.NoValidEventException;
+import eventprocessing.agent.NoValidTargetTopicException;
 import eventprocessing.agent.interestprofile.AbstractInterestProfile;
 import eventprocessing.event.AbstractEvent;
+import eventprocessing.utils.factory.LoggerFactory;
 import eventprocessing.utils.model.EventUtils;
 
 
@@ -21,6 +27,30 @@ public class Tokens extends AbstractInterestProfile {
 	@Override
 	protected void doOnReceive(AbstractEvent event) {
 		
+		if(EventUtils.isType("GuiEvent", event)) {
+			Logger l = LoggerFactory.getLogger("logger");
+			l.log(Level.WARNING, "GUIEVENT ERHALTEN");
+			try {
+				this.getAgent().send(event, "DocProposal");
+			} catch (NoValidEventException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoValidTargetTopicException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		try {
+			this.getAgent().send(event, "test");
+		} catch (NoValidEventException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoValidTargetTopicException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Das Event "+event+" in Tokens IP angekommen");
 		if(EventUtils.findPropertyByKey(event, "contextupdate") != null) {
 			System.out.println("Das Event "+event+" ist ein CONTEXTUPDATE");
