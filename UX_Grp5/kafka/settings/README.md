14df875b833a49a86678afb4da2f607416f7fcfb
markus <markus@141.62.95.206>
2018-06-27 12:52:03 +0200

    UX_Grp5 eingefügt

---
+++ UX_Grp5/kafka/settings/README.md
@@ -0,0 +1,4 @@
+# Erläuterung
+
+Die ProducerSettings beinhalten die nötigen Parameter für die Verbindung zu dem Kafkaserver.
+Die ProducerSettingsValues beinhalten die entsprechenden Werte für die Parameter.
---
+++ UX_Grp5/src/eventprocessing/agent/GuiAgent/GuiInterestProfile.java
@@ -0,0 +1,93 @@
+package eventprocessing.agent.GuiAgent;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import eventprocessing.demo.ShowcaseValues;
+import eventprocessing.events.DocProposalEvent;
+import eventprocessing.events.GuiEvent;
+import eventprocessing.events.SessionEndEvent;
+import eventprocessing.demo.events.SensorEvent;
+import eventprocessing.event.AbstractEvent;
+import eventprocessing.utils.TextUtils;
+import eventprocessing.utils.factory.LoggerFactory;
+
+import eventprocessing.agent.NoValidEventException;
+import eventprocessing.agent.NoValidTargetTopicException;
+import eventprocessing.agent.interestprofile.AbstractInterestProfile;
+import eventprocessing.demo.ShowcaseValues;
+import eventprocessing.event.AbstractEvent;
+import eventprocessing.event.Property;
+import eventprocessing.utils.factory.AbstractFactory;
+import eventprocessing.utils.factory.FactoryProducer;
+import eventprocessing.utils.factory.FactoryValues;
+import eventprocessing.utils.factory.LoggerFactory;
+import eventprocessing.utils.model.EventUtils;
+import values.GUIValues;
+import eventprocessing.demo.ShowcaseValues;
+import eventprocessing.demo.events.SensorEvent;
+import eventprocessing.event.AbstractEvent;
+import eventprocessing.utils.TextUtils;
+import eventprocessing.utils.factory.LoggerFactory;
+
+/**
+ * Dieses Interessenprofil führt die Reinigung sowie Filterung durch. Es wird
+ * nach fehlerhaften Nachrichten ausschau gehalten, die aus dem Datenstrom
+ * gefiltert werden, bevor diese an den <code>GuAgent</code> Agenten
+ * geschickt werden.
+ * 
+ * @author IngoT
+ *
+ */
+public class GuiInterestProfile extends AbstractInterestProfile {
+
+	private static final long serialVersionUID = -210735813565569965L;
+	private static Logger LOGGER = LoggerFactory.getLogger(GuiInterestProfile.class);
+
+	/**
+	 * Verarbeitung des empfangenen Events.
+	 * 
+	 * @param event
+	 */
+	@Override
+	public void doOnReceive(AbstractEvent event) {
+			
+		System.out.println("doOnReceive");
+		GuiEvent e = (GuiEvent) event;
+			
+				try {
+					getAgent().send(e, "Gui");
+					System.out.println("getAgent");
+				} catch (NoValidEventException e1) {
+					System.out.println("NogetAgent");
+					LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+				} catch (NoValidTargetTopicException e1) {
+					LOGGER.log(Level.WARNING, () -> String.format("%s", "Gui"));
+				}
+				
+				System.out.println("Consumer");
+				Property<?> gui=EventUtils.findPropertyByKey(e, "name");
+				System.out.println(gui);
+			
+		
+		
+	/*	
+		// Prüfung ob es vom Typ SensorEvent ist.
+		if (event instanceof SensorEvent) {
+			// Wenn ja, cast.
+			SensorEvent e = (SensorEvent) event;
+			// Aussortierung von fehlerhaften Sensorwerten.
+			if (e.getSensorID() != 0 && !TextUtils.isNullOrEmpty(e.getLocation())) {
+				try {
+					// Das erzeugte Event wird über den Agenten an das Topic "TrafficData" versendet
+					getAgent().send(e, ShowcaseValues.INSTANCE.getTrafficDataTopic());
+				} catch (NoValidEventException e1) {
+					LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+				} catch (NoValidTargetTopicException e1) {
+					LOGGER.log(Level.WARNING, () -> String.format("%s", e));
+				}
+			}
+		} */
+	}
+
+}
